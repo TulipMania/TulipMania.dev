ceb8826c500544a20efb8da7b809fe0d387668cd
Pablo Pinero <pinerop@gmail.com>
2015-05-11 15:45:06 -0500

    added two views for adventures..all text

---
+++ app/views/adventure_template_two.blade.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>tulipmania</title>
+</head>
+<body>
+	<h1>You Chose: YOUR CHOICE</h1>
+	<br>
+	<h3>So here is what will happen:</h3>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	<p>Good luck!</p>
+	<a href="#">Go back to field.</a>
+</body>
+</html>
