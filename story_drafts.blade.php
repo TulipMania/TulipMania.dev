370386d1f194c20f2f763097679b5f74a05c7916
Pablo Pinero <pinerop@gmail.com>
2015-05-12 14:45:17 -0500

    worked on story drafts and created ScenariosSeeder

--- story_drafts.blade.php
+++ story_drafts.blade.php
@@ -1,7 +1,6 @@
 Valkenswaard, Low Countries
 January 9, 1629
 
-1.
 Groundskeeper Hennie says: "Good news, $characterName! Your mean great uncle, Rinus, died of bloody flux and left his tulip field to you! 
 
 "As you know, tulips are of great value in this land. Grow good tulips and we --ahem, I mean, you-- could become a very rich man.
@@ -26,16 +25,23 @@ POSSIBLE INITIAL SCENARIOS AT THE GROUNDS:
 		AC. Arjen drowned! You lose a day fishing his body out of the river. 	(But you get his ƒ3.00!) 
 	B. You run into your old friend Bert van Praag from your days at the University of Groningen. He and his wife are on their way to a party at Count Kiesel's. Would you like to join them?
 	Hennie says: "Make sure to give the driver a ƒ1.50 gratuity. You don't want to get a reputation for being a miser."
-		BA. The count's wife has taken a liking to you. She asks if you 		would like to pair up with her for a short minuet. From across 		the room the count gives you a look. He is not happy.  
-			Would you like to dance with her?
-				BAA. Oh no! Count Kiesel has challenged you to a duel! If you accept you can get seriously hurt... or even DIE! But if you decline your reputation will suffer and you will lose ƒ9.00 in sales. (No one wants to buy tulips from a coward!) 
-					BAAA.  
-				BAB. 
-				BAC.
-		BB.
-		BC.
+		BA. The countess has taken a liking to you. She asks if you 		would like to pair up with her for a short minuet. From across 		the room the count gives you a look. He is not happy.  
+		Would you like to dance with her?
+			BAA. Oh no! Count Kiesel has challenged you to a duel! If you accept you can get seriously hurt... or even DIE! But if you decline, your reputation will suffer and you will lose upwards of ƒ9.00 in sales. (No one wants to buy tulips from a coward!) 
+				BAAA. The Count has shot you in the knee! You lose 
+				three days of rest and ƒ12.30 in medical fees. 
+				BAAB. The Count shot you in the face! The countess gives you mouth to mouth but, alas, you are dead. 
+				BAAC. You have shot and killed the count! 
+				The Countess says says: "Thank you so much. I hated that man. Please accept these SPECIAL SEEDS as a token of my appreciation."   
+			BAB. The Count winks at you! He respects a man who will dance with the Countess. You gain a reputation for being a courageous man. People want to do business with you. You have an extra ƒ5.90.  
+		BB. A man at the party by the name of Djavan says he's a childhood friend of yours from Den Burg. You don't recognize him, but he seems nice. 
+		Djavan says: "I have some top-shelf opium with me. Would you like to smoke some out in the woods?"
+			BBA. In a great opium dream you stumble into the drawing room and win ƒ12.00 at the poker table while speaking in tongues. 
+			BBB. Oh no! You had a very interesting opium dream in which you vomited birds of all sorts...but you also lost your wallet with ƒ5.00 inside. 
+			BBC. Djavan was actually a con artist! While you're in the middle of an opium dream about flying giant beetles, he steals ƒ8.00 from your pocket. 
+			BBD. During an opium dream about Rinus you finally remember who Djavan is! You two hit it off and you even manage to sell him ƒ8.00 worth of tulips.  
 
-	C.
+	
 
 3. TAKE THE STAGECOACH TO EINDHOVEN. 
 
