5447d75990211b3bc7cc8b4fa6f96d1deff4394b
Pablo Pinero <pinerop@gmail.com>
2015-05-13 13:35:56 -0500

    worked on scenarios

--- story_drafts.blade.php
+++ story_drafts.blade.php
@@ -14,8 +14,6 @@ if(yes){
 }
 
 2. TAKE A STROLL AROUND THE GROUNDS.
-
-POSSIBLE INITIAL SCENARIOS AT THE GROUNDS:
 	A. Arjen, the town drunk, bets you ƒ3.00 that he can beat you swimming across the river. 
 	Hennie says: "Be careful, master. Arjen, though a lush, is a magnificent swimmer. Also, the water is very cold. On the other hand, those three guilders could buy some much needed $itemBelowThreeGuilders."  
 		AA. You lose!
@@ -40,14 +38,37 @@ POSSIBLE INITIAL SCENARIOS AT THE GROUNDS:
 			BBB. Oh no! You had a very interesting opium dream in which you vomited birds of all sorts...but you also lost your wallet with ƒ5.00 inside. 
 			BBC. Djavan was actually a con artist! While you're in the middle of an opium dream about flying giant beetles, he steals ƒ8.00 from your pocket. 
 			BBD. During an opium dream about Rinus you finally remember who Djavan is! You two hit it off and you even manage to sell him ƒ8.00 worth of tulips.  
+	C. You come across a cave that looks oh so much like one in which you used to play as a child. You decide to go in. A bear!\n Do you want to fight it or try to tame it? \n
+	Hennie says: "I will meet you back at the house, master."
+		CA. (IF TAME) You fool! This bear cannot be tamed. You lose two fingers and ƒ11.00 in medical fees. 
+		CB. (IF FIGHT) Those morning push-ups have paid off! You kill the bear and sell its skin for ƒ14.00.
+		CC. (IF TAME) You sing the bear a lullaby and it goes to sleep. You walk out silently. 
+		CD. (IF FIGHT) You are able to land a few punches, but the bear bit your ear off! Spend ƒ12.00 in medical fees. 
+	D. You come across the ghost of your uncle Rinus! Hennie doesn't see anything, he thinks you're crazy. \n
+	Uncle Rinus says: \"My boy! I hope you're enjoying my tulip field. You need money? Buy ƒ19.87 worth of lottery tickets! trust me! Uncle Rinus, out!\" \n
+	Do you want to buy the lottery tickets?
+		DA. None of your tickets were winners. Apparently you are going insane from all that time working with tulips. Spend three days in the sanatorium and lose ƒ19.87. 
+		DB. Uncle Rinus out? More like Uncle Rinus IN! You win ƒ390.78 and you make fun of Hennie for doubting you.
+		DC. Only one of your tickets comes out a winner. You pocket ƒ6.54. Was it really due to Rinus or pure luck?
 
-	
-
-3. TAKE THE STAGECOACH TO EINDHOVEN. 
-
-	A.
-	B.
-	C.
-
+3. TAKE THE STAGECOACH TO EINDHOVEN.  
+	A. While drinking in a tavern, an old veteran of the tulip fields lets you in on an old and very controversial new tulip-growing strategy. Would you like to implement it? 
+		AA. Damn old man! Jealous of your youth and prospects, he took pleasure in tricking you! Lose ƒ25.05.   
+		AB. The strategy worked! Pocket ƒ29.09.
+		AC. The wise old man turned out to be the town idiot. His strategy for growing tulips affects your fields in absolutely no way. 
+	B. While at a coffeehouse, a scientist approaches you. He'd like you to invest in wind energy. \n
+	Hennie says: \"Windmills are witchcraft\" \n
+	Would you like to invest ƒ31.87 in windmills?
+		BA. YES! Your investment returns ƒ69.51.
+		BB. Apparently Hennie was right. The windmills must be witchcraft. They all burnt down. You lose ƒ31.87
+		BC. Your investment gives a modest return of ƒ9.01.
+	C. An employee of the Dutch East India Company drops his coin pouch and you find it. \n
+	Would you like to return it?
+		CA. No! That man's rich enough!
+			CAA. The police arrest you for theft. Lose three days and ƒ20.91 in fines. 
+			CAB. You pocket the ƒ30.00 in the pouch. 
+		CB. Of course! Praise God!
+			CBA. The man says: \"Thank you, peasant. Now please leave and take your foul smell with you.\"
+			CBB. The man thanks you and buys you drinks. Pocket ƒ11.93.
 
 4. TAKE THE WAGONWAY TO AMSTERDAM.
