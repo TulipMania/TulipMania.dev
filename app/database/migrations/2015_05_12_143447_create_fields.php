067e32996e0a5c9e2b60612a7772c47870d69b90
Ryan Dinesman <rdinesman@gmail.com>
2015-05-14 11:34:54 -0500

    fixed scenarios migration functionality. added getFromSID to Scenrio model. made intial changes to showAdventureTemplate

--- app/database/migrations/2015_05_12_143447_create_fields.php
+++ app/database/migrations/2015_05_12_143447_create_fields.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFieldsDb extends Migration {
+class CreateFields extends Migration {
 
 	/**
 	 * Run the migrations.
