acdbcdeb68f316aa871684f3a3754530f981cea5
J.D. Garza <jdgarza22@yahoo.com>
2015-05-15 10:48:13 -0500

    -changed redirect

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -63,10 +63,10 @@ public function __construct()
 		$password   = Input::get('password');
 
 		if (Auth::attempt(array('username' => $user_input, 'password' => $password))) {
-			return Redirect::intended('HomeController@showField');
+			return Redirect::action('HomeController@showField');
 		}
 		else if (Auth::attempt(array('email' => $user_input, 'password' => $password))) {
-			return Redirect::intended('HomeController@showField');	
+			return Redirect::action('HomeController@showField');	
 		}else{
 			Session::flash('errorMessage','Incorrect email or password');
 			return Redirect::back()->withInput();
