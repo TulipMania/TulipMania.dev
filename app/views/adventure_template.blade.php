8519de54dffc2681f91e93e01119faccd1fce63f
J.D. Garza <jdgarza22@yahoo.com>
2015-05-20 13:47:03 -0500

    changed controller from home to game

--- app/views/adventure_template.blade.php
+++ app/views/adventure_template.blade.php
@@ -4,23 +4,20 @@
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	</head>
     <body>
+
     	<div id="money"> 
 			<i class="fa fa-money"></i> ƒ{{{ Auth::user()->money }}}
 		</div>
 	        <div class="container" id="story">
 
 	        @if($story_id == "s_grounds" || $story_id == "s_intro" )
-	        	<a href="{{{ action('HomeController@showField')}}}"><i class="fa fa-home"></i></a>
+	        	<a href="{{{ action('GameController@showField')}}}"><i class="fa fa-home"></i></a>
 	        @endif
 
-	        @if($story_id == "s_intro")
-	           	<p>{{{ substr($body, 0,138).Auth::user()->username.substr($body, 151,422)}}}</p>
-	        @else
-	        	<p>{{{$body}}}</p>
-	        @endif
-	              <a href="{{{ action('HomeController@showAdventureTemplate', [$nextScenario]) }}}">{{{ $next_headers }}}</a>
+	           	<p>{{$body}}</p>
+
+	              <a href="{{{ action('GameController@showAdventureTemplate', [$nextScenario]) }}}">{{{ $next_headers }}}</a>
 	           	<br>
-	        </div>
+	        </div> 
     </body>
 </html>
-
